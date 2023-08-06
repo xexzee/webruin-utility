5b120e2a6a42ab6109909825729f06584e663fdb
xexzee <xexzee@gmail.com>
2023-08-06 00:59:31 -0400

    add item deletion functionality

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv').config();
-const {MongoClient} = require('mongodb');
+const {MongoClient, ObjectId} = require('mongodb');
 const fs = require('fs');
 const readline = require('readline').createInterface({input: process.stdin, output: process.stdout});
 const {Storage} = require('@google-cloud/storage');
@@ -12,23 +12,33 @@ const typesWithAURL = ['archived-audio', 'archived-image', 'software', 'screensh
 const typesWithCreators = ['software'];
 const typesWithMultipleFiles = ['physical', 'software'];
 const typesWithDisplayDimensions = ['archived-image', 'software', 'screenshot', 'phyisical'];
+const actions = ['catalog', 'delete', 'exit'];
+
+let mongoClient = null;
+let mongoCollection = null;
+let gcsBucket = null;
+
+async function initializeConnections() {
+
+    // initialize database
+    mongoClient = new MongoClient(process.env.MONGODB_URI);
+    await mongoClient.connect();
+    mongoCollection = mongoClient.db(process.env.MONGODB_DATABASE).collection(process.env.MONGODB_COLLECTION);
+
+    // initialize gcs
+    let gcs = new Storage();
+    gcsBucket = gcs.bucket(process.env.GCS_BUCKETNAME);
+
+}
 
 async function catalog_items() {
 
     // read items in staging directory
     let stagedItems = fs.readdirSync(process.env.STAGING_PATH).filter(itemName => itemName !== '.gitignore' && itemName !== 'desktop.ini');
     if(stagedItems.length === 0) {
-        console.log(redify('STAGIN DIRECTORY IS EMPTY; NO ITEMS TO BE CATALOGED'));
+        console.log(redify('STAGING DIRECTORY IS EMPTY; NO ITEMS TO BE CATALOGED'));
         process.exit();
     }
-
-    // initialize database
-    const mongoClient = new MongoClient(process.env.MONGODB_URI);
-    await mongoClient.connect();
-    const mongoCollection = mongoClient.db(process.env.MONGODB_DATABASE).collection(process.env.MONGODB_COLLECTION);
-
-    // initialize gcs
-    const storage = new Storage();
 
     while(stagedItems.length > 0) {
 
@@ -50,7 +60,7 @@ async function catalog_items() {
             while(true) {
                 if(types.includes(answer))
                     return resolve(answer);
-                answer = await new Promise(resolve => readline.question(magentify('TYPE (' + types.toString() + '): '), answer => resolve(answer)));
+                answer = await new Promise(resolve => readline.question(magentify('TYPE (' + types.toString().replaceAll(',', ', ') + '): '), answer => resolve(answer)));
             }
         }));
 
@@ -154,8 +164,6 @@ async function catalog_items() {
             }));
         }
 
-        
-
         // verify that all data looks correct
         let redoItem = await new Promise(resolve => readline.question(cyanify('FINAL DATA TO BE ADDED:\n' + JSON.stringify(data, null, 4)) + magentify('\nDOES THIS LOOK CORRECT? (y/n): '), async answer => {
             while(true) {
@@ -163,7 +171,7 @@ async function catalog_items() {
                     return resolve(false);
                 else if(answer.toLowerCase().trim() === 'n' || answer.toLowerCase().trim() === 'no')
                     return resolve(true);
-                answer = await new Promise(resolve => readline.question('(y/n): ', answer => resolve(answer)));
+                answer = await new Promise(resolve => readline.question(magentify('(y/n): '), answer => resolve(answer)));
             }
         }));
 
@@ -181,7 +189,7 @@ async function catalog_items() {
             // upload item files to the gcs bucket
             for(filename of data.filenames) {
                 console.log(cyanify('UPLOADING ' + filename + ' TO GCS BUCKET...'));
-                await storage.bucket(process.env.GCS_BUCKETNAME).upload(process.env.STAGING_PATH + filename, {destination: data._id + '/' + filename});
+                await gcsBucket.upload(process.env.STAGING_PATH + filename, {destination: data._id + '/' + filename});
                 console.log(cyanify('UPLOAD SUCCESSFUL!'));
             }
 
@@ -205,7 +213,6 @@ async function catalog_items() {
     }
 
     console.log(cyanify('ALL ITEMS CALATOGED!! YAY!!! (´ω｀^=)~'));
-    process.exit();
 }
 
 function cyanify(string) {
@@ -220,4 +227,133 @@ function redify(string) {
     return '\x1b[31m' + string + '\x1b[0m';
 }
 
-catalog_items();
+async function main() {
+
+    // start program
+    let running = true;
+    while(running) {
+
+        // prompt for action
+        let action = await new Promise(resolve => readline.question(magentify('ACTION: '), async answer => {
+            while(true) {
+                if(actions.includes(answer))
+                    return resolve(answer);
+                answer = await new Promise(resolve => readline.question(magentify('ACTION (' + actions.toString().replaceAll(',', ', ') + '): '), answer => resolve(answer)));
+            }
+        }));
+
+        // initialize connections
+        initializeConnections();
+
+        // execute action
+        if(action === 'catalog')
+            await catalog_items();
+        else if(action === 'delete')
+            await delete_items();
+        else if(action === 'exit')
+            break;
+
+        // prompt to continue
+        running = await new Promise(resolve => readline.question(magentify('CONTINUE WITH ANOTHER ACTION? (y/n): '), async answer => {
+            while(true) {
+                if(answer.toLowerCase().trim() === 'y' || answer.toLowerCase().trim() === 'yes') 
+                    return resolve(true);
+                else if(answer.toLowerCase().trim() === 'n' || answer.toLowerCase().trim() === 'no')
+                    return resolve(false);
+                answer = await new Promise(resolve => readline.question(magentify('(y/n): '), answer => resolve(answer)));
+            }
+        }));
+    }
+
+    // end the program's execution
+    console.log(cyanify('BAII~!!! ⸜(｡> ᵕ < )⸝'));
+    await mongoClient.close();
+    process.exit();
+}
+
+async function delete_items() {
+
+    let deleting = true;
+    while(deleting) {
+
+        // prompt for item id
+        let itemId = await new Promise(resolve => readline.question(magentify('ITEM ID: '), async answer => {
+            while(true) {
+                try {
+                    let itemId = new ObjectId(answer);
+                    return resolve(itemId);
+                } catch(e) {
+                    answer = await new Promise(resolve => readline.question(magentify('ITEM ID: (must be of valid object id format: a string of 12 bytes or 24 hex characters, or an integer): '), answer => resolve(answer)));
+                }
+            }
+        }));
+
+        console.log(cyanify('FETCHING ITEM...'));
+        let item = await mongoCollection.findOne({_id: itemId});
+        let continuePrompt = null;
+        if(item) {
+            console.log(cyanify('ITEM FOUND!'));
+            let confirmDeletion = await new Promise(resolve => readline.question(magentify('DELETE ITEM "' + item.name + '"? (y/n): '), async answer => {
+                while(true) {
+                    if(answer.toLowerCase().trim() === 'y' || answer.toLowerCase().trim() === 'yes') 
+                        return resolve(true);
+                    else if(answer.toLowerCase().trim() === 'n' || answer.toLowerCase().trim() === 'no')
+                        return resolve(false);
+                    answer = await new Promise(resolve => readline.question(magentify('(y/n): '), answer => resolve(answer)));
+                }
+            }));
+            if(confirmDeletion) {
+                if(!itemId || !itemId.toString() || itemId.toString() === '/' || !itemId.toString().trim()) {
+                    console.log(redify('ITEM ID WAS FOUND TO BE "' + itemId.toString() + '" RIGHT BEFORE DELETION, RECHECK PROGRAM CODE AND/OR ITEM ID'));
+                    process.exit();
+                }
+                console.log(cyanify('DELETING ITEM DATA...'));
+                await mongoCollection.deleteOne({_id: itemId});
+                console.log(cyanify('ITEM DATA SUCCESSFULLY DELETED!'));
+                console.log(cyanify('DELETING ITEM FILES FROM GCS...'));
+                let gcsFiles = (await gcsBucket.getFiles({ prefix: itemId.toString() + '/' }))[0];
+                let numberOfDeletedFilesFromGcs = 0;
+                let fileDeletionPromises = [];
+                for(const file of gcsFiles) {
+                    fileDeletionPromises.push(new Promise((resolve, reject) => {
+                        file.delete((error, response) => {
+                            if(error) {
+                                console.log(redify('ERROR ENCOUNTERED WHEN TRYING TO DELETE FILE "' + '", FILE WAS NOT LIKELY NOT PROPERLY DELETED'));
+                                console.log(redify('FULL ERROR:'));
+                                console.log(error);
+                            }
+                            else {
+                                console.log(cyanify('FILE "' + file.name + '" WAS SUCCESSFULLY DELETED!'));
+                                numberOfDeletedFilesFromGcs++;
+                            }
+                            resolve();
+                        });
+                    }));
+                }
+                await Promise.all(fileDeletionPromises);
+                console.log(cyanify(numberOfDeletedFilesFromGcs + ' FILE(S) SUCCESSFULLY DELETED FROM GCS!'));
+                console.log(cyanify('DELETING ITEM FILES FROM LOCAL STORAGE...'));
+                let filesSavedLocally = fs.readdirSync(process.env.CATALOGED_PATH + itemId.toString());
+                fs.rmSync(process.env.CATALOGED_PATH + itemId.toString(), {recursive: true, force: true});
+                console.log(cyanify(filesSavedLocally.length + ' FILE(S) DELETED FROM LOCAL STORAGE SUCCESFULLY! ("' + filesSavedLocally.toString().replaceAll(',', '", "') + '")'));
+            }
+            continuePrompt = magentify('CONTINUE WITH A NEW ITEM ID? (y/n): ');
+        }
+        else {
+            console.log(redify('NO ITEM WITH ID ' + itemId + ' WAS FOUND TO EXIST...'));
+            continuePrompt = magentify('TRY AGAIN WITH A DIFFERENT ITEM ID? (y/n): ');
+        }
+
+        deleting = await new Promise(resolve => readline.question(continuePrompt, async answer => {
+            while(true) {
+                if(answer.toLowerCase().trim() === 'y' || answer.toLowerCase().trim() === 'yes') 
+                    return resolve(true);
+                else if(answer.toLowerCase().trim() === 'n' || answer.toLowerCase().trim() === 'no')
+                    return resolve(false);
+                answer = await new Promise(resolve => readline.question(magentify('(y/n): '), answer => resolve(answer)));
+            }
+        }));
+    }
+}
+
+main();
